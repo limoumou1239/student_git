d3cb2fbe9bb9c108a60daecfa6d68e09185b14ea
limoumou <limoumou@itcast.cn>
2025-09-27 22:41:30 +0800

    第一次保存

---
+++ PID.c
@@ -0,0 +1,138 @@
+#include <stdio.h> 
+#include <windows.h>
+#include <stdlib.h>
+typedef struct
+{
+	float Kp;
+	float Ki;
+	float Kd;
+	float last_error; // 上一次的误差
+	float integral;   // 积分累积值
+	float last_number;
+	float error_max; //误差最大值
+	float integral_max; //积分最大值
+	int outcome_max; //输出最大值
+}PID;
+
+void PID_Init(PID* Pid, float Kp, float Ki, float Kd, float error_max, float integral_max, int outcome_max)
+{
+	Pid->Kp = Kp;
+	Pid->Ki = Ki;
+	Pid->Kd = Kd;
+	Pid->last_error = 0.0; // 初始化上一次的误差为0
+	Pid->integral = 0.0;   // 初始化积分累积值为0
+	Pid->last_number = 0.0; // 初始化上一次的 number 值为 0
+	Pid->error_max = error_max;
+	Pid->integral_max = integral_max;
+	Pid->outcome_max = outcome_max;
+
+}
+
+int PID_General_Cal(PID* Pid, float tar, float number)
+{
+	float error = (tar - number); //这一次误差
+	if (error > Pid->error_max)
+	{
+		error = Pid->error_max;
+	}
+	else if (error < -Pid->error_max)
+	{
+		error = -Pid->error_max;
+	}
+
+	int outcome;
+
+	Pid->integral += error;
+	if ((Pid->last_number < tar && number >= tar) || (Pid->last_number > tar && number <= tar))
+	{
+		Pid->integral = 0.0;
+	}
+	Pid->last_number = number;
+	if (Pid->integral > Pid->integral_max)
+	{
+		Pid->integral = Pid->integral_max;
+	}
+	else if (Pid->integral < -Pid->integral_max)
+	{
+		Pid->integral = -Pid->integral_max;
+	}
+
+	float d = error - Pid->last_error;
+	outcome = Pid->Kp * error + Pid->Ki * Pid->integral + Pid->Kd * d;
+	Pid->last_error = error; //更新上一次误差
+	if (outcome > Pid->outcome_max)
+	{
+		outcome = Pid->outcome_max;
+	}
+	else if (outcome < -Pid->outcome_max)
+	{
+		outcome = -Pid->outcome_max;
+	}
+	return outcome;
+}
+
+PID pid1;
+PID pid2;
+PID pid3;
+
+int main()
+{
+	float number1 = 0, tar1 = 4000;
+	int number2 = 0, tar2 = -4000;
+	int number3 = 0, tar3 = 8000;
+
+	PID_Init(&pid1, 18, 0, 0, 4000, 8000.0, 20000);
+	PID_Init(&pid2, 5, 0.0135, 0, 4000, 8000.0, 20000);
+	PID_Init(&pid3, 5, 0.1, 0, 8000, 8000.0, 20000);
+
+	while (1)
+	{
+		number1 += 0.1 * PID_General_Cal(&pid1, tar1, number1);
+		number1 -= 10;
+		printf("现在的值为： %lf\n", number1);
+		Sleep(200);//windows.h 中的延时函数,观察数值变化过程。
+	}
+	system("CLS"); //#include stdlib.h 中清屏函数
+
+	while (number2 != tar2)
+	{
+		number2 += 0.1 * PID_General_Cal(&pid2, tar2, number2);
+		number2 += 10;
+		printf("现在的值为： %d\n", number2);
+		Sleep(200);//windows.h 中的延时函数,观察数值变化过程。
+	}
+	system("CLS"); //#include stdlib.h 中清屏函数
+
+	while (number3 != tar3)
+	{
+		number3 -= 10;
+		static int i = 0;
+		i++;
+		if (PID_General_Cal(&pid3, tar3, number3) > 20000)
+		{
+			printf("你的无人机因电机转速过快导致无人机坠毁！！！ ");
+			break;
+		}
+
+		number3 += 0.1 * PID_General_Cal(&pid3, tar3, number3);
+		if (i < 20)
+		{
+			number3 = 0;
+		}
+
+		printf("现在的值为： %d\n", number3);
+		if (number3 > 10000)
+		{
+			printf("你的无人机撞到了房顶！！！ ");
+			break;
+		}
+		if (i > 100)
+		{
+			printf("你的无人机因电量用光，坠毁！！！ ");
+			break;
+		}
+
+		Sleep(100);//windows.h 中的延时函数,观察数值变化过程。
+	}
+	return 0;
+}
---
+++ shaolei.c
@@ -0,0 +1,174 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#define X 10
+#define Y 10
+int map[X + 2][Y + 2];
+int record[X + 2][Y + 2];
+int x, y;
+char choice[100];
+void START() {
+    srand((unsigned int)time(NULL));
+    for (int i = 0; i < X + 2; i++) {
+        for (int j = 0; j < Y + 2; j++) {
+            map[i][j] = 0;
+            record[i][j] = 0;
+        }
+    }
+}
+
+void BURY() {
+    int sum;
+    printf("请输入雷的数量：");
+    scanf("%d", &sum);
+    while (sum <= 0 || sum >= 100) {
+        printf("输入错误！\n");
+        printf("请输入雷的数量：");
+        scanf("%d", &sum);
+    }
+    printf("输入要点击的格子坐标（行 列）:");
+    scanf("%d %d", &x, &y);
+    int a = 0;
+    while (a < sum) {
+        int m = rand() % X + 1;
+        int n = rand() % Y + 1;
+        if (map[m][n] == 0 && (m != x || n != y)) {
+            map[m][n] = -1;
+            a++;
+        }
+    }
+}
+
+void NUMBER() {
+    for (int i = 1; i <= X; i++) {
+        for (int j = 1; j <= Y; j++) {
+            if (map[i][j] != -1) {
+                int mines = 0;
+                for (int b = i - 1; b <= i + 1; b++) {
+                    for (int c = j - 1; c <= j + 1; c++) {
+                        if (map[b][c] == -1 && (b != i || c != j)) {
+                            mines++;
+                        }
+                    }
+                }
+                map[i][j] = mines;
+            }
+        }
+    }
+}
+
+void PRINT() {
+    for (int i = 0; i <= X; i++) {
+        for (int j = 0; j <= Y; j++) {
+            if (i == 0 && j > 0) {
+                printf("%2d ", j);
+            }
+            else if (j == 0 && i > 0) {
+                printf("%2d ", i);
+            }
+            else if (record[i][j]) {
+                if (map[i][j] == -1) {
+                    printf(" * ");
+                }
+                else {
+                    printf("%2d ", map[i][j]);
+                }
+            }
+            else {
+                printf(" - ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+void RECURSION(int i, int j) {
+    if (i < 1 || i > X || j < 1 || j > Y || record[i][j] || map[i][j] != 0) {
+        return;
+    }
+    record[i][j] = 1;
+    RECURSION(i + 1, j);
+    RECURSION(i - 1, j);
+    RECURSION(i, j + 1);
+    RECURSION(i, j - 1);
+}
+
+int main() {
+    char a[100];
+    while (1) {
+        printf("是否开始游戏（开始/退出）:");
+        scanf("%s", a);
+        if (strcmp(a, "开始") == 0) {
+            START();
+            BURY();
+            NUMBER();
+            if (x < 1 || x > X || y < 1 || y > Y) {
+                printf("无效的坐标，请重新输入。\n");
+            }
+            else {
+                record[x][y] = 1;
+                printf("周围有 %d 个地雷\n", map[x][y]);
+                if (map[x][y] == 0) {
+                    RECURSION(x, y);
+                }
+                PRINT();
+                while (1) {
+                    printf("输入要点击的格子坐标（行 列）:");
+                    scanf("%d %d", &x, &y);
+                    if (x < 1 || x > X || y < 1 || y > Y) {
+                        printf("无效的坐标，请重新输入。\n");
+                    }
+                    else if (record[x][y]) {
+                        printf("这个格子已经被点过了！\n");
+                    }
+                    else {
+                        record[x][y] = 1;
+                        if (map[x][y] == -1) {
+                            printf("Boom!恭喜你踩到地雷了！\n游戏结束。\n");
+                            PRINT();
+                            break;
+                        }
+                        else {
+                            printf("周围有 %d 个地雷\n", map[x][y]);
+                            if (map[x][y] == 0) {
+                                RECURSION(x, y);
+                            }
+                            PRINT();
+                        }
+                    }
+                    int allCleared = 1;
+                    for (int i = 1; i <= X; i++) {
+                        for (int j = 1; j <= Y; j++) {
+                            if (map[i][j] != -1 && !record[i][j]) {
+                                allCleared = 0;
+                                break;
+                            }
+                        }
+                    }
+                    if (allCleared) {
+                        printf("恭喜，你赢了！\n");
+                        break;
+                    }
+                }
+                printf("要再来一次吗？（再来一次/退出游戏）：");
+                scanf("%s", choice);
+                if (strcmp(choice, "退出游戏") == 0) {
+                    printf("再见\n");
+                    break;
+                }
+                else if (strcmp(choice, "再来一次") == 0) {
+                    continue;
+                }
+            }
+        }
+        else if (strcmp(a, "退出") == 0) {
+            printf("再见\n");
+            break;
+        }
+        else {
+            printf("输入错误！\n");
+        }
+    }
+    return 0;
+}
